fc6e90d9f26a0ced4833b3d3e2570d5135b29e4e
Francis dela Cruz <franciszac.dlc@gmail.com>
2017-11-29 23:40:40 +0800

    Fix UI and routes of Regional Data Records

--- app/controllers/regional_data_records_controller.rb
+++ app/controllers/regional_data_records_controller.rb
@@ -1,10 +1,11 @@
 class RegionalDataRecordsController < ApplicationController
+  before_action :set_region
   before_action :set_regional_data_record, only: [:show, :edit, :update, :destroy]
 
   # GET /regional_data_records
   # GET /regional_data_records.json
   def index
-    @regional_data_records = RegionalDataRecord.all
+    @regional_data_records = @region.regional_data_records.all
   end
 
   # GET /regional_data_records/1
@@ -14,7 +15,7 @@ class RegionalDataRecordsController < ApplicationController
 
   # GET /regional_data_records/new
   def new
-    @regional_data_record = RegionalDataRecord.new
+    @regional_data_record = @region.regional_data_records.new
   end
 
   # GET /regional_data_records/1/edit
@@ -24,11 +25,11 @@ class RegionalDataRecordsController < ApplicationController
   # POST /regional_data_records
   # POST /regional_data_records.json
   def create
-    @regional_data_record = RegionalDataRecord.new(regional_data_record_params)
+    @regional_data_record = @region.regional_data_records.new(regional_data_record_params)
 
     respond_to do |format|
       if @regional_data_record.save
-        format.html { redirect_to @regional_data_record, notice: 'Regional data record was successfully created.' }
+        format.html { redirect_to [@region, @regional_data_record], notice: 'Regional data record was successfully created.' }
         format.json { render :show, status: :created, location: @regional_data_record }
       else
         format.html { render :new }
@@ -42,7 +43,7 @@ class RegionalDataRecordsController < ApplicationController
   def update
     respond_to do |format|
       if @regional_data_record.update(regional_data_record_params)
-        format.html { redirect_to @regional_data_record, notice: 'Regional data record was successfully updated.' }
+        format.html { redirect_to [@region, @regional_data_record], notice: 'Regional data record was successfully updated.' }
         format.json { render :show, status: :ok, location: @regional_data_record }
       else
         format.html { render :edit }
@@ -56,19 +57,22 @@ class RegionalDataRecordsController < ApplicationController
   def destroy
     @regional_data_record.destroy
     respond_to do |format|
-      format.html { redirect_to regional_data_records_url, notice: 'Regional data record was successfully destroyed.' }
+      format.html { redirect_to region_regional_data_records_url, notice: 'Regional data record was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
+    def set_region
+      @region = Region.find(params[:region_id])
+    end
     def set_regional_data_record
       @regional_data_record = RegionalDataRecord.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def regional_data_record_params
-      params.require(:regional_data_record).permit(:region_id, :release_date, :acadyr, :cost_per_stud, :private_nonsec, :private_sec, :special_HEI, :other_govt_schools, :ched_supervised_institution, :local_colleges_and_uni, :SUCs_main, :integer, :SUCs_sat)
+      params.require(:regional_data_record).permit(:region_id, :release_date, :acadyr, :cost_per_stud, :private_nonsec, :private_sec, :special_HEI, :other_govt_schools, :ched_supervised_institution, :local_colleges_and_uni, :SUCs_main, :SUCs_sat)
     end
 end

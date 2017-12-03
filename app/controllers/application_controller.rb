bb3c41527e109efc1d1343225d3d5e0e83037977
Francis dela Cruz <franciszac.dlc@gmail.com>
2017-12-03 23:39:34 +0800

    Fix Principal signup and TESDA Course form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
+  #before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
+
+=begin
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :p_name, :institution_id, :password, :password_confirmation, :current_password])
+  end
+=end
 end
+
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,10 +3,10 @@ class RegistrationsController < Devise::RegistrationsController
   private
 
   def sign_up_params
-    params.require(:principal).permit(:email, :p_name, :password, :password_confirmation)
+    params.require(:principal).permit(:email, :p_name, :institution_id, :password, :password_confirmation, :current_password)
   end
 
   def account_update_params
-    params.require(:principal).permit(:email, :p_name, :password, :password_confirmation, :current_password)
+    params.require(:principal).permit(:email, :p_name, :institution_id, :password, :password_confirmation, :current_password)
   end
 end
